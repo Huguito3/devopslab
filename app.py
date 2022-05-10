e15cb22a3be2c02f302e6c7f4d526cfa5a281a11
Huguito3 <hduraniroz@gmail.com>
2022-05-09 21:06:50 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def pagina_inicial():
+#     return "Hola Mundo - Hugo, Turma Full 03"
+
+# if __name__ == '__main__':
+#     app.run()
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "Hola Mundo - Hugo, Turma Full 03"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run()
