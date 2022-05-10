073d634833ab9b22616da0b521d64cb3c3c2bfa2
Huguito3 <hduraniroz@gmail.com>
2022-05-09 21:48:07 -0300

    removido comentarios

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def pagina_inicial():
-#     return "Hola Mundo - Hugo, Turma Full 03"
-
-# if __name__ == '__main__':
-#     app.run()
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
